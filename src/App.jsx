07e2b667cbb4f2ab36eab56438f263c28925a236
Sara Rampazzo <sararampazzo.ph@gmail.com>
2025-02-18 23:49:14 +0100

    feat: Created InputField component for email and password

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
-function App() {
-  return <></>;
+import React from "react";
+import InputField from "./InputField"; // Assicurati che il percorso sia corretto
+
+export function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <InputField
+        label="E-mail or username"
+        id="username"
+        type="text"
+        placeholder="E-mail or username"
+      />
+      <InputField
+        label="Password"
+        id="password"
+        type="password"
+        placeholder="Password"
+      />
+    </div>
+  );
 }
 
 export default App;
