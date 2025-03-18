db4317e29f437a197a16f02503a847b6581df386
Clara-newbie <m.clara.massaro@gmail.com>
2025-03-18 19:08:29 +0100

    feat: inizio creazione componente

---
+++ src/components/NewStory_1.jsx
@@ -0,0 +1,15 @@
+import BackButton from "./BackButton";
+
+export default function NewStory_1() {
+  return (
+    <div>
+      <BackButton pageUrl="" />; {/* DEVI COLLEGARE IL PATH */}
+      {/* LoadImage */}
+      {/* InputField */}
+      {/* InputField */}
+      {/* SelectGenres */}
+      {/* BackButton */}
+      {/* StartWritingButton */}
+    </div>
+  );
+}
