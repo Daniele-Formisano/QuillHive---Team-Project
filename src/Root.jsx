3547e599c35f439e60d711e9db2a03d0bc5091d0
Antonio Buzzo <antoniobuzzo002@gmail.com>
2025-03-19 17:18:21 +0100

    fix: switch login to app in root component

--- src/Root.jsx
+++ src/Root.jsx
@@ -3,14 +3,13 @@ import { BrowserRouter } from "react-router-dom";
 import { store } from "./app/store";
 import App from "./App";
 import { Toaster } from "react-hot-toast";
-import Login from "./pages/Login";
 
 export default function Root() {
   return (
     <BrowserRouter>
       <Provider store={store}>
         <Toaster />
-        <Login />
+        <App />
       </Provider>
     </BrowserRouter>
   );
