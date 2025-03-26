7cf12f8e3596a4fd3a8f4766981ceb570078c0f6
Manuel Lomaglio <manuelomaglio@gmail.com>
2025-03-26 15:47:43 +0100

    Fix & feat: fix card,search, navbar and created home page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,35 +1,51 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useGetStoriesQuery } from "../services/apiService";
 
+function Card({ story }) {
+  // const stories = data.stories.slice(0,1);//riduzio dell'array, fetch momentaneo
+  //   const { data: stories, error, isLoading } = useGetStoriesQuery();
 
+  //   if (isLoading) return <p>Loading</p>;
+  //   if (error) return <p>Error </p>;
+  //   if (!stories || stories.length === 0) return <p>No stories</p>;
 
-function Card() {
-  // const stories = data.stories.slice(0,1);//riduzio dell'array, fetch momentaneo 
-  const { data: stories, error, isLoading } = useGetStoriesQuery();
+  //  const limitedStories= stories.slice(0,7);
 
-  if (isLoading) return <p>Loading</p>;
-  if (error) return <p>Error </p>;
-  if (!stories || stories.length === 0) return <p>No stories</p>;
+  const [showTooltip, setShowTooltip] = useState(false);
 
-  useEffect;
   return (
     <div>
-      <ul>
-        {stories.map((story) => (
-          <li key={story.id} className="w-[150px] h-[290px] border-transparent rounded-2xl flex flex-col items-center">
-            <img
-              src={story.cover_image}
-              alt="Book cover"
-              width={150}
-              height={230}
-              className="border-transparent rounded-2xl "
-            />
-            <p className="font-medium text-[16px] text-secondary-brand ">{story.title}</p>
-            <p className="font-light text-[12px] text-secondary-brand secondary-brand">George Orwell</p> 
-            {/* momentaneo perchè autore sarebbe user_id */}
-          </li>
-        ))}
-      </ul>
+      {/* <ul>
+        {stories.map((story) => ( */}
+      <li
+        key={story.id}
+        className="w-[150px] h-[290px] border-transparent rounded-2xl flex flex-col items-center mt-8 relative">
+        <img
+          src={story.cover_image}
+          alt="Book cover"
+          width={150}
+          height={230}
+          className="border-transparent rounded-2xl "
+        />
+        <p
+          className="font-medium text-[16px] text-center text-secondary-brand truncate w-[100px] mt-3.5"
+          onMouseEnter={() => setShowTooltip(true)}
+          onMouseLeave={() => setShowTooltip(false)}>
+          {story.title}
+          {showTooltip && (
+            <span className="absolute bottom-10 bg-black text-white p-1 text-xs rounded z-50">
+              {story.title}
+            </span>
+          )}
+        </p>
+
+        <p className="font-light font text-[12px] text-secondary-brand secondary-brand mt-2">
+          {story.userId}
+        </p>
+        {/* momentaneo perchè autore sarebbe user_id */}
+      </li>
+
+      {/* </ul> */}
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,106 @@
+import Card from "../components/Card";
 import Navbar from "../components/navbar";
 import Searchbar from "../components/Searchbar";
+import { useGetStoriesQuery, useGetUsersQuery } from "../services/apiService";
 
 //div Header (hamburger menu, search, profile)
 //travel in the hive(div( map of componet that displys authors))
 // buzzing for you (no display if not logged) (div(map of card book component))
 //hive's choice (div(map of card book component))
-// footer navbar z index position absolute
-
+// footer navbar z index position fixed
 
 function Home() {
+  // const{data:users, isLoading, error}=useGetUsersQuery();
+  // if (isLoading) return <p>Loading</p>;
+  //   if (error) return <p>Error </p>;
+  //   if (!stories || stories.length === 0) return <p>No users</p>;
+
+  const {
+    data: stories,
+    error: storiesError,
+    isLoading: storiesLoading,
+  } = useGetStoriesQuery();
+
+  if (storiesLoading) return <p>Loading</p>;
+  if (storiesError) return <p>Error </p>;
+  if (!stories || stories.length === 0) return <p>No stories</p>;
+
+  const limitedStories = stories.slice(0, 7);
+
   return (
-    <div className="flex flex-col">
-      <Searchbar />
-      <Navbar />
+    <div className="flex flex-col justify-center  bg-bg-brand ">
+      <header className="flex flex-row justify-center items-center fixed top-0 left-0 right-0 bg-bg-brand">
+        <button className="w-9 h-9 ">H</button>
+        <Searchbar />
+        <button className="w-[54px] h-[54]">P</button>
+      </header>
+      <main className=" flex flex-col justify-center overflow-y-scroll pl-5 pb-16 pt-16 scrollbar-hide ">
+        <div className="flex flex-row gap-4 mb-5 pt">
+          <p className="text-secondary-brand font-title text-center text-2xl ">
+            Travel in the hive
+          </p>
+          {/* <ul>{users.map((user)=>( <li>
+            </li>
+
+          ))}</ul> */}
+        </div>
+
+        <div className="flex flex-col justify-center">
+          <p className="text-secondary-brand font-title text-2xl">Buzzing for you</p>
+          <div className="">
+            <ul className="flex flex-row gap-4 overflow-x-scroll space-x-4 snap-x snap-mandatory scrollbar-hide ">
+              {limitedStories.map((story) => (
+                <Card key={story.id} story={story} />
+              ))}
+            </ul>
+          </div>
+        </div>
+        <div className="flex flex-col justify-center">
+          <p className="text-secondary-brand font-title text-2xl">Hive's choices</p>
+          <div className="">
+            <ul className="flex flex-row gap-5 overflow-x-scroll space-x-4 snap-x snap-mandatory scrollbar-hide  ">
+              {limitedStories.map((story) => (
+                <Card key={story.id} story={story} />
+              ))}
+            </ul>
+          </div>
+        </div>
+        <div className="flex flex-col justify-center mb-10">
+          <p className="text-secondary-brand font-title text-2xl ">Trending now</p>
+          <div className="">
+            <ul className="flex flex-row gap-4 overflow-x-scroll space-x-4 snap-x snap-mandatory scrollbar-hide ">
+              {limitedStories.map((story) => (
+                <Card key={story.id} story={story} />
+              ))}
+            </ul>
+          </div>
+        </div>
+        <div className="flex flex-col justify-center mb-10">
+          <p className="text-secondary-brand font-title text-2xl ">Collab Spotlight</p>
+          <div className="">
+            <ul className="flex flex-row gap-4 overflow-x-scroll space-x-4 snap-x snap-mandatory scrollbar-hide ">
+              {limitedStories.map((story) => (
+                <Card key={story.id} story={story} />
+              ))}
+            </ul>
+          </div>
+        </div>
+        <div className="flex flex-col justify-center mb-10">
+          <p className="text-secondary-brand font-title text-2xl ">QuillHive Originals</p>
+          <div className="">
+            <ul className="flex flex-row gap-4 overflow-x-scroll space-x-4 snap-x snap-mandatory scrollbar-hide ">
+              {limitedStories.map((story) => (
+                <Card key={story.id} story={story} />
+              ))}
+            </ul>
+          </div>
+        </div>
+        <footer>Footer</footer>
+      </main>
+      <div className=" flex justify-center fixed font-title bottom-0 left-0 right-0 z-50">
+        <Navbar/>
+      </div>
     </div>
   );
-}export default Home;
+}
+export default Home;  
