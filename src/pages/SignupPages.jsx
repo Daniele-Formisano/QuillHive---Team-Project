6f0f7c842204902ff7a62276b07273bd5177afdc
Antonio Buzzo <antoniobuzzo002@gmail.com>
2025-04-15 18:24:40 +0200

    add user in localstorage in signup

--- src/pages/SignupPages.jsx
+++ src/pages/SignupPages.jsx
@@ -86,6 +86,7 @@ export default function SignupPages({ genres, artistTypes }) {
               };
 
               await AddUserArtistTypes(userArtistType);
+              localStorage.setItem("user", JSON.stringify(newUser)); // salva l'utente nel local storage
             }
           }
         } catch (error) {
